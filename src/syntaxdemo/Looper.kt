8b3e278efff8de1ff7ea2d013bf742c01ae541d4
Haozhe Xu <jamesxuhaozhe@gmail.com>
2018-12-27 15:54:40 +0800

    Add more kotlin feature examples

--- src/syntaxdemo/Looper.kt
+++ src/syntaxdemo/Looper.kt
@@ -174,5 +174,18 @@ fun parsePath1(path: String) {
     }
 }
 
+class User(val id: Int, val name: String, val address: String)
+
+fun saveUser(user: User) {
+    fun validate(user: User, value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw java.lang.IllegalArgumentException("Can't save user: ${user.id} empty $fieldName")
+        }
+    }
+    validate(user, user.name, "name")
+    validate(user, user.address, "address")
+}
+
+
 
 
