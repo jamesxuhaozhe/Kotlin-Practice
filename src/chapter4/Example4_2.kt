8ed520cc976ef4494aa4c8670fc1ea9880e21e8d
Haozhe Xu <jamesxuhaozhe@gmail.com>
2019-01-04 17:25:08 +0800

    Add more class related examples

--- src/chapter4/Example4_2.kt
+++ src/chapter4/Example4_2.kt
@@ -1,6 +1,8 @@
 package chapter4
 
 import java.io.Serializable
+import java.util.jar.Attributes
+import javax.naming.Context
 
 // public
 // as a class element, meaning it can be seen everywhere
@@ -45,3 +47,105 @@ class Outer {
         fun getOuterString(): String = this@Outer.getString()
     }
 }
+
+class User constructor(_nickname: String) {
+    val nickname: String
+
+    init {
+        nickname = _nickname
+    }
+}
+
+class User1(_age: Int) {
+    val age = _age
+}
+
+open class User2(val nickname: String)
+
+class User3(val nickname: String, val isMale: Boolean = true)
+
+val user = User("James")
+
+class TwitterUser(nickname: String) : User2(nickname) {
+
+}
+
+open class Button1  // you can declare a class just like this, it doesn't do shit 
+
+class RadioButton : Button1()    // a subclass that doesn't have its own
+// ctor, you need to put () in the superclass
+
+
+class Secretive private constructor() {
+
+}
+
+open class MyView {
+    constructor(ctx: Context) {
+        // no-op
+    }
+
+    constructor(ctx: Context, attr: Attributes) {
+        
+    }
+}
+
+class MyButton1 : MyView {
+    constructor(ctx: Context): super(ctx) {
+
+    }
+    constructor(ctx: Context, attr: Attributes): super(ctx, attr) {
+        
+    }
+}
+
+class Bird {
+    constructor(sex: String, age: Int)
+
+    constructor(sex: String): this(sex, 1) {
+
+    }
+}
+
+interface IUser {
+    val nickname: String
+}
+
+class PrivateUser(override val nickname: String) : IUser
+
+class SubscribingUser(val email: String) : IUser {
+    override val nickname: String
+        get() = email.substringBefore('@')
+}
+
+class FacebookUser(val accountId: Int) : IUser {
+    override val nickname: String = getFacebookName(accountId)
+
+    fun getFacebookName(accountId: Int): String = "nickname"
+}
+
+val privateUser = PrivateUser("James")
+
+interface IUser2 {
+    val email: String
+    val nickname: String
+        get() = email.substringBefore('@')
+}
+
+class MyUser(val name: String) {
+    var address: String = "unspecified"
+        set(value: String) {
+            println("""
+                Address was changed for $name: "$field" -> "$value"
+            """.trimIndent())
+            field = value // field is essentially for address
+            // field you can modify it in setter but you cannot do it
+            // in getter
+        }
+}
+
+val user2 = MyUser("Alice")
+
+
+
+
