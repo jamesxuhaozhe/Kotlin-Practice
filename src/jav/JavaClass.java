1592f57d90c8050f0ea42323526e90c367a982b5
Haozhe Xu <jamesxuhaozhe@gmail.com>
2019-01-15 13:29:42 +0800

    Java 8 lambda lazy eval

--- src/jav/JavaClass.java
+++ src/jav/JavaClass.java
@@ -3,7 +3,6 @@ package jav;
 import chapter5.Milk;
 import strings.StringFunctions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +21,18 @@ public class JavaClass {
         List<Milk> milks = Arrays.asList(new Milk("james", 1), new Milk("hsfsdf", 3), new Milk("you", 6));
 
 
+        List<Integer> lists = Arrays.asList(1, 2, 3, 4, 54, 8, 10, 34, 5, 6, 44);
 
+        lists.stream().map(JavaClass::mapFunc).filter(JavaClass::predicateLogic).toArray();
+    }
+
+    private static Integer mapFunc(Integer integer) {
+        System.out.println("Map " + integer);
+        return integer * integer;
+    }
+
+    private static boolean predicateLogic(Integer integer) {
+        System.out.println("filter " + integer);
+        return integer > 20;
     }
 }
